09da5761fd4c20a18c7dd7fe42e990258fcbd010
BrianChanLEE <brianlee1914@me.com>
2024-09-09 19:38:31 +0900

    utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -32,14 +32,13 @@ const generateSchedule = (startTime, people) => {
     // 참여자가 8명일 경우 15분 간격, 4명 이상 8명 이하일 경우 20분 간격, 4명 이하일 경우 30분 간격 설정
     if (people.length === 8) {
         intervalMinutes = 15;
-    } else if (people.length > 8) {
-        return { error: "참여자 수가 너무 많습니다. 최대 8명까지만 허용됩니다." };
-    } else if (people.length >= 4) {
+    } else if (people.length >= 4 && people.length < 8) {
         intervalMinutes = 20;
-    } else {
+    } else if (people.length < 4) {
         intervalMinutes = 30;
+    } else {
+        return { error: "참여자 수가 너무 많습니다. 최대 8명까지만 허용됩니다." };
     }
-
     // 사람 배열을 무작위로 섞음
     shuffleArray(people);
 
