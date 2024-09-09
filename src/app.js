50fdc61a19155a49af802774888839b9f578fa3d
BrianChanLEE <brianlee1914@me.com>
2024-09-09 17:35:59 +0900

    app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ app.use((err, req, res, next) => {
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.get('*', (req, res) => {
+    res.status(404).send('Page not found');
+});
 
 // 스케줄 관련 라우트 사용
 app.use('/', scheduleRoutes);
