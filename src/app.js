712afe49d54af8f553ea7f3fba0a2df4d6eb7431
BrianChanLEE <brianlee1914@me.com>
2024-09-09 23:00:19 +0900

    app.js

--- src/app.js
+++ src/app.js
@@ -14,12 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 
 
-// 정적 파일 서빙 설정
-app.use(express.static('public'));
+
 // 스케줄 관련 라우트 사용
 app.use('/', scheduleRoutes);
 
