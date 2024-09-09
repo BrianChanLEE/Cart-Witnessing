cfd77bba2b72343e3220015c76d7c27562aae869
BrianChanLEE <brianlee1914@me.com>
2024-09-09 16:02:58 +0900

    vercel.json

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const app = require('./src/app');
 const port = 4040;
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
