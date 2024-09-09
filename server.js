33a0d9066242c2c4a3ecb84d7dce6ca815b60e43
BrianChanLEE <brianlee1914@me.com>
2024-09-09 16:18:41 +0900

    vercel error

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 const app = require('./src/app');
 const port = 4040;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
